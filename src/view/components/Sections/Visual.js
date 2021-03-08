6cbf075ac8c3861db0f7c72fa735ea79c2abebee
suyeon <tndus7644@gmail.com>
2021-03-08 19:53:39 +0900

    visual background

--- src/view/components/Sections/Visual.js
+++ src/view/components/Sections/Visual.js
@@ -16,7 +16,7 @@ const Visual = () => {
 
 const Container = styled.div`
   height: 100vh;
-  background: #fff url('https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80') 50% / cover no-repeat;
+  background: #fff url('https://images.unsplash.com/photo-1562619371-b67725b6fde2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80') 50% / cover no-repeat;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -26,7 +26,7 @@ const Content = styled.div`
   position: relative;
     h1{
       font-size: 55px;
-      color: #18f;
+      color: #fff;
       text-transform: capitalize;
     }
 `;
