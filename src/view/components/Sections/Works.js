c714e401ec6d3c6910ef7dc8e86685e6f7f4f2ed
tndus7644 <tndus7644@gmail.com>
2023-08-17 17:34:25 +0900

    image change

--- src/view/components/Sections/Works.js
+++ src/view/components/Sections/Works.js
@@ -10,35 +10,35 @@ const works = [
         name:"Unsplash",
         description:"Unsplash API를 이용한 클론 프로젝트",
         url:"https://brave-jones-b7aaa8.netlify.app/",
-        imgSrc:"https://images.unsplash.com/photo-1569605803663-e9337d901ff9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80",
+        imgSrc:"https://images.unsplash.com/photo-1502882705085-fd1fd2ecefd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1212&q=80",
         tags:["react", "redux saga", "redux", "styled components"]
     },
     {
         name:"Youtube",
         description:"Unsplash API를 이용한 클론 프로젝트",
         url:"https://suspicious-jang-e29a61.netlify.app/",
-        imgSrc:"https://images.unsplash.com/photo-1567443024551-f3e3cc2be870?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imgSrc:"https://images.unsplash.com/photo-1521302200778-33500795e128?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         tags:["react", "redux saga", "redux", "styled components"]
     },
     {
         name:"Twitch",
         description:"twitch API를 이용한 클론 프로젝트",
         url:"https://reverent-blackwell-9c6a2a.netlify.app/",
-        imgSrc:"https://images.unsplash.com/photo-1527334919515-b8dee906a34b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imgSrc:"https://images.unsplash.com/photo-1527334919515-b8dee906a34b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         tags:["react", "redux saga", "redux", "styled components"]
     },
     {
         name:"Github",
         description:"제 github 사이트입니다.",
         url:"https://github.com/tndus7644",
-        imgSrc:"https://images.unsplash.com/photo-1591453089343-9ee5e4ac7e2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imgSrc:"https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80",
         tags:[]
     },
     {
         name:"Blog",
         description:"공부 기록용 블로그입니다.",
         url:"https://gudoc.tistory.com/",
-        imgSrc:"https://images.unsplash.com/photo-1519337265831-281ec6cc8514?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imgSrc:"https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         tags:[]
     },
     {
